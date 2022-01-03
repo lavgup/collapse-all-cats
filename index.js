128030e44f5f19b85dca421903ae7ff9e2ba9d07
lavgup <lavyag01@gmail.com>
2022-01-03 20:14:06 +1100

    Fix + add icon

--- index.js
+++ index.js
@@ -3,49 +3,52 @@ const { Plugin } = require('powercord/entities');
 const { findInReactTree } = require('powercord/util');
 const { inject, uninject } = require('powercord/injector');
 const {
-    getModule,
-    React,
-    i18n: { Messages }
+	getModule,
+	React,
+	i18n: { Messages }
 } = require('powercord/webpack');
+const CategoriesIcon = require('./components/CategoriesIcon');
 
-const press = new KeyboardEvent('keydown', { which: 65, keyCode: 65, shiftKey: true, ctrlKey: true, bubbles: true });
+const press = new KeyboardEvent('keydown', { which: 65, keyCode: 65, shiftKey: true, metaKey: true, bubbles: true });
 Object.defineProperties(press, { keyCode: { value: 65 }, which: { value: 65 } });
 
 class CollapseAllCats extends Plugin {
-    async startPlugin() {
-        powercord.api.i18n.loadAllStrings(i18n);
-        await this.injectMenu();
-    }
+	async startPlugin() {
+		powercord.api.i18n.loadAllStrings(i18n);
+		await this.injectMenu();
+	}
 
-    async injectMenu() {
-        const Menu = await getModule(['MenuItem']);
+	async injectMenu() {
+		const Menu = await getModule(['MenuItem']);
 
-        inject('collapse-cats-menu', Menu, 'default', ([{ children }], res) => {
-            if (res.props.id !== 'guild-header-popout') return res;
+		inject('collapse-cats', Menu, 'default', ([{ children }], res) => {
+			if (res.props.children.props.id !== 'guild-header-popout') return res;
 
-            if (!findInReactTree(res, c => c.props && c.props.id === 'collapse-cats-menu')) {
-                children.unshift(
-                    React.createElement(Menu.MenuGroup, null, React.createElement(Menu.MenuItem, {
-                        id: 'collapse-cats-menu',
-                        label: Messages.COLLAPSE_CATEGORIES,
-                        action: () => this.collapseCategories()
-                    }))
-                );
-            }
-            return res;
-        });
+			if (!findInReactTree(res, c => c.props && c.props.id === 'collapse-cats-menu')) {
+				children.push(
+					React.createElement(Menu.MenuGroup, null, React.createElement(Menu.MenuItem, {
+						id: 'collapse-cats-menu',
+						label: Messages.COLLAPSE_CATEGORIES,
+						icon: () => React.createElement(CategoriesIcon),
+						action: () => this.collapseCategories()
+					}))
+				);
+			}
 
-        Menu.default.displayName = 'Menu';
-    }
+			return res;
+		});
 
-    collapseCategories() {
-        document.body.dispatchEvent(press);
-    }
+		Menu.default.displayName = 'Menu';
+	}
 
-    pluginWillUnload() {
-        uninject('collapse-cats-menu');
-        uninject('collapse-cats-context-menu');
-    }
+	collapseCategories() {
+		document.body.dispatchEvent(press);
+	}
+
+	pluginWillUnload() {
+		uninject('collapse-cats-menu');
+		uninject('collapse-cats-context-menu');
+	}
 }
 
 module.exports = CollapseAllCats;
